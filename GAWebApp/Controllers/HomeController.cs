dd5fab869cc5019a57238beff48bb3fe4ff66ed8
SpencerParkin <spencerparkin@outlook.com>
2019-08-04 21:50:12 -0600

    remove azure blob storage dependency to hopefully make me asp.net core compliant

--- GAWebApp/Controllers/HomeController.cs
+++ GAWebApp/Controllers/HomeController.cs
@@ -7,8 +7,6 @@ using System.Xml.Linq;
 using System.Threading.Tasks;
 using System.Runtime.Caching;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 using GAWebApp.Models;
 using GeometricAlgebra;
 
@@ -58,23 +56,6 @@ namespace GAWebApp.Controllers
             return PartialView("HistoryView", state);
         }
 
-        private CloudBlobContainer GetCloudBlobContainer()
-        {
-            // Here's the connection string in plain text, which is not recommended.
-            // I couldn't find the Azure namespace, though, so until then, here it is.
-            // I guess it will be part of the git history too.
-            string connectionString = "DefaultEndpointsProtocol=https;AccountName=gawebappstorage;AccountKey=tWQkquTxPG8y+7BsfQ8tNajEsdfxrK0o4v6h1DWhyOPY84ZyP3ZAHiLtifso4Hsy10gUJ4K0uPM228ACKEja+w==;EndpointSuffix=core.windows.net";
-            //string connectionString = CloudConfigurationManager.GetSetting("gawebappstorage_AzureStorageConnectionString");
-
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
-
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer container = blobClient.GetContainerReference("ga-calc-container");
-
-            return container;
-        }
-
-#if true
         private State GetState(string calculatorID)
         {
             return defaultState;
@@ -83,91 +64,5 @@ namespace GAWebApp.Controllers
         private void SetState(string calculatorID, State state)
         {
         }
-#else
-
-        private State GetState(string calculatorID)
-        {
-            Task<State> task = GetStateAsync(calculatorID);
-            //task.Wait(5000);
-            task.Wait();
-
-            if(task.IsCompletedSuccessfully)
-                return task.Result;
-
-            return defaultState;
-        }
-
-        private async Task<State> GetStateAsync(string calculatorID)
-        {
-            if(calculatorID.Length == 0)
-                return defaultState;
-
-            State state = MemoryCache.Default.Get(calculatorID) as State;
-            if(state != null)
-                return state;
-
-            try
-            {
-                CloudBlobContainer container = GetCloudBlobContainer();
-                await container.CreateIfNotExistsAsync();
-
-                CloudBlockBlob blob = container.GetBlockBlobReference(calculatorID);
-
-                state = new State();
-
-                try
-                {
-                    string blobXml = await blob.DownloadTextAsync();
-                    XElement rootElement = XElement.Parse(blobXml);
-                    state.DeserializeFromXml(rootElement);
-                }
-                catch(Exception)        // What's the specific exception?
-                {
-                    XElement rootElement = new XElement("State");
-                    state.context.GenerateDefaultStorage();
-                    state.SerializeToXml(rootElement);
-                    string blobXml = rootElement.ToString();
-                    await blob.UploadTextAsync(blobXml);
-                }
-                
-                MemoryCache.Default.Set(calculatorID, state, null);
-
-                return state;
-            }
-            catch(Exception exc)
-            {
-                string error = exc.Message;
-
-                return defaultState;
-            }
-        }
-
-        private async void SetState(string calculatorID, State state)
-        {
-            // The caller should be free to continue while the task waits on the upload operation.
-            await SetStateAsync(calculatorID, state);
-        }
-
-        private async Task SetStateAsync(string calculatorID, State state)
-        {
-            try
-            {
-                CloudBlobContainer container = GetCloudBlobContainer();
-                await container.CreateIfNotExistsAsync();
-
-                CloudBlockBlob blob = container.GetBlockBlobReference(calculatorID);
-
-                XElement rootElement = new XElement("State");
-                state.SerializeToXml(rootElement);
-                string blobXml = rootElement.ToString();
-                await blob.UploadTextAsync(blobXml);
-            }
-            catch(Exception exc)
-            {
-                string error = exc.Message;
-                //...
-            }
-        }
-#endif
     }
 }
